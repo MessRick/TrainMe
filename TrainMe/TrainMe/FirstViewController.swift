934e2cdd61a76ad33bb7bf32e52e1cb9c43cda2a
MessRick <32860410+MessRick@users.noreply.github.com>
2018-09-17 20:00:54 +0300

    Add files via upload

---
+++ TrainMe/TrainMe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TrainMe
+//
+//  Created by Арсений Живаго on 17.09.2018.
+//  Copyright © 2018 Арсений Живаго. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
